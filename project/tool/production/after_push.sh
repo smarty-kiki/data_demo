51c358004bf6c3b41c3d5edc407a0880af36fb14
yaoyang <kiki@smarty.so>
2020-06-28 18:22:06 +0800

    Fork from git@github.com:smarty-kiki/api_frame.git and modify project name

--- project/tool/production/after_push.sh
+++ project/tool/production/after_push.sh
@@ -2,13 +2,13 @@
 
 ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../..
 
-ln -fs $ROOT_DIR/project/config/production/nginx/api_frame.conf /etc/nginx/sites-enabled/api_frame
+ln -fs $ROOT_DIR/project/config/production/nginx/data_demo.conf /etc/nginx/sites-enabled/data_demo
 /usr/sbin/service nginx reload
 
 /bin/bash $ROOT_DIR/project/tool/dep_build.sh link
 /usr/bin/php $ROOT_DIR/public/cli.php migrate:install
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
 
-ln -fs $ROOT_DIR/project/config/production/supervisor/api_frame_queue_worker.conf /etc/supervisor/conf.d/api_frame_queue_worker.conf
+ln -fs $ROOT_DIR/project/config/production/supervisor/data_demo_queue_worker.conf /etc/supervisor/conf.d/data_demo_queue_worker.conf
 /usr/bin/supervisorctl update
-/usr/bin/supervisorctl restart api_frame_queue_worker:*
+/usr/bin/supervisorctl restart data_demo_queue_worker:*
